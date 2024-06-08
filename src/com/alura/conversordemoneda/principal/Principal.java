8e2c94206eb3dc15db6ca765110bb2af0c451d27
Ingrid Pedraza <ingridpedraza0815@gmail.com>
2024-06-08 06:09:31 +0000

    crear menu

--- src/com/alura/conversordemoneda/principal/Principal.java
+++ src/com/alura/conversordemoneda/principal/Principal.java
@@ -1,8 +1,43 @@
 package com.alura.conversordemoneda.principal;
 
+import java.util.Scanner;
+
 public class Principal {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in) ;
+        int option  = 0 ;
+        String menu  = """
+             **************************************
+             Bienvenido al conversor de monedas\n
+             1.) Dólar =>> Peso argentino
+             2.) Peso argentino = >> Dólar
+             3.) Dólar = >> Real brasileño
+             4.) Real brasileño = >> dólar
+             5.) Dólar = >> Peso colombiano
+             6.) Peso colombiano = >> Dólar
+             7.) Salir
 
+             **************************************
+            """ ;
+
+        while (option != 7) {
+            System . out . println(menu);
+            System . out . println ( "Elija una opción" );
+            option = sc.nextInt();
+
+         switch (option){
+                case 1 :
+                case 2 :
+                case 3 :
+                case 4 :
+
+                case 5 :
+                case 6 :
+
+
+            }
+
+        }
     }
 
 }
